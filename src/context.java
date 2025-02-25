dda9c14d52ffaf479eef7eb8fb482e0ee25728ec
qbakom <qbakom@gmail.com>
2025-02-25 21:33:11 +0100

    Add conversation class for managing chat history

---
+++ src/context.java
@@ -0,0 +1,20 @@
+package context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConversationContext {
+    private final List<Message> history = new ArrayList<>();
+
+    public void addMessage(Message message) {
+        history.add(message);
+    }
+
+    public String getContextAsString() {
+        StringBuilder sb = new StringBuilder();
+        for (Message msg : history) {
+            sb.append(msg.getAuthor()).append(": ").append(msg.getContent()).append("\n");
+        }
+        return sb.toString();
+    }
+}
